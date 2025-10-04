127149869909b4f5538d72d31beee42db82d5db0
Muhammad Faizan Aziz <165632955+Faizan-Aziz@users.noreply.github.com>
2025-10-05 00:04:06 +0500

    Update socket.jsx

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,6 +1,6 @@
 import {io} from 'socket.io-client';
 
-const socket = io('https://hope-by-aziz-serverapp.onrender.com', {
+const socket = io('https://chat-app-server-ulsb.onrender.com', {
   transports: ['polling']
 })
 
