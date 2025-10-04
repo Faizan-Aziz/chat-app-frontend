93c740abb782436ced2961046de3db4631af545f
Muhammad Faizan Aziz <165632955+Faizan-Aziz@users.noreply.github.com>
2025-10-04 23:42:05 +0500

    Update socket.jsx

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,7 +1,7 @@
 import {io} from 'socket.io-client';
 
-const socket = io('ws://localhost:8000', {
-  transports: ['polling']
+const socket = io('https://hope-by-aziz-serverapp.onrender.com', {
+  transports: ['websocket']
 })
 
 export default socket;
